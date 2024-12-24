5e277b2dc25ee69c6ababc59138206b4b8641f4d
drozdov <drozdov.dn29@gmail.com>
2024-12-24 23:23:39 +0300

    fix docker compose and file backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -30,6 +30,6 @@ async function bootstrap() {
       groups: ['default'],
     }),
   );
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.BACKEND_PORT || 3000);
 }
 bootstrap();
